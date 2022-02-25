038399370709b454f96da33896081949283c88de
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2022-02-25 15:06:49 +0530

    added file transfer component
    added file transfer component

---
+++ src/app/file-transfer-window/file-transfer-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-transfer-window',
+  templateUrl: './file-transfer-window.component.html',
+  styleUrls: ['./file-transfer-window.component.scss']
+})
+export class FileTransferWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
